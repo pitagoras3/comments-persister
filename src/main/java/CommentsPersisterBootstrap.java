539743eee35ef527301b7fcfa50b1fce58247651
pitagoras3 <szymon.marcinkiewicz@gmail.com>
2019-02-04 20:48:37 +0100

    Implemented getting n comments as JsonObjects

--- src/main/java/CommentsPersisterBootstrap.java
+++ src/main/java/CommentsPersisterBootstrap.java
@@ -1,2 +1,15 @@
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.ext.web.client.WebClient;
+
 public class CommentsPersisterBootstrap {
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        WebClient webClient = WebClient.create(vertx);
+
+        CommentsReceiver commentsReceiver = new CommentsReceiver(webClient);
+        commentsReceiver.getComments(3)
+                .doFinally(vertx::close)
+                .subscribe(json -> System.out.println(json.encodePrettily()));
+    }
 }
--- src/main/java/CommentsReceiver.java
+++ src/main/java/CommentsReceiver.java
@@ -1,2 +1,29 @@
+import io.reactivex.Observable;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.ext.web.client.HttpResponse;
+import io.vertx.reactivex.ext.web.client.WebClient;
+
+import java.util.List;
+import java.util.Map;
+
 public class CommentsReceiver {
+
+    private static final String COMMENT_API_URL = "https://jsonplaceholder.typicode.com/comments";
+
+    private final WebClient webClient;
+
+    public CommentsReceiver(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Observable<JsonObject> getComments(int amountOfComments) {
+        return Observable.range(1, amountOfComments)
+                .flatMapSingle(id -> webClient.getAbs(COMMENT_API_URL)
+                        .addQueryParam("postId", String.valueOf(id))
+                        .rxSend())
+                .map(HttpResponse::bodyAsJsonArray)
+                .map(x -> (List<Map>)x.getList())
+                .flatMapIterable(x -> x)
+                .map(JsonObject::new);
+    }
 }
