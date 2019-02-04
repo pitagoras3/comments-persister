4918caeef047c0d77aa86e72fdaaf7a880878b58
pitagoras3 <szymon.marcinkiewicz@gmail.com>
2019-02-04 23:16:34 +0100

    Implemented first working version

--- src/main/java/CommentsPersisterBootstrap.java
+++ src/main/java/CommentsPersisterBootstrap.java
@@ -1,7 +1,11 @@
 import io.reactivex.Observable;
+import io.reactivex.Single;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.client.WebClient;
+
+import java.util.Collection;
+import java.util.Map;
 
 public class CommentsPersisterBootstrap {
 
@@ -9,10 +13,17 @@ public class CommentsPersisterBootstrap {
         Vertx vertx = Vertx.vertx();
         WebClient webClient = WebClient.create(vertx);
 
-        CommentsReceiver commentsReceiver = new CommentsReceiver(webClient);
+        CommentsReceiver commentsReceiver = new CommentsReceiver(vertx, webClient);
         Observable<JsonObject> comments = commentsReceiver.getComments(3);
-        commentsReceiver.groupCommentsByDomain(comments)
+        Single<Map<String, Collection<JsonObject>>> extensionToJsons = commentsReceiver.groupCommentsByDomain(comments);
+
+        extensionToJsons.map(Map::keySet)
+                .flattenAsObservable(x -> x)
+                .flatMapCompletable(commentsReceiver::createDirectoryForDomain)
+                .andThen(extensionToJsons)
+                .flattenAsObservable(Map::entrySet)
+                .flatMapCompletable(entry -> commentsReceiver.writeCommentsToFile(entry.getKey(), entry.getValue()))
                 .doFinally(vertx::close)
-                .subscribe(map -> map.forEach((x, y) -> System.out.println(x + y)));
+                .subscribe();
     }
 }
--- src/main/java/CommentsReceiver.java
+++ src/main/java/CommentsReceiver.java
@@ -1,6 +1,9 @@
+import io.reactivex.Completable;
 import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.core.buffer.Buffer;
 import io.vertx.reactivex.ext.web.client.HttpResponse;
 import io.vertx.reactivex.ext.web.client.WebClient;
 
@@ -11,10 +14,13 @@ import java.util.Map;
 public class CommentsReceiver {
 
     private static final String COMMENT_API_URL = "https://jsonplaceholder.typicode.com/comments";
+    private static final String COMMENTS_PATH = "comments/";
 
+    private final Vertx vertx;
     private final WebClient webClient;
 
-    public CommentsReceiver(WebClient webClient) {
+    public CommentsReceiver(Vertx vertx, WebClient webClient) {
+        this.vertx = vertx;
         this.webClient = webClient;
     }
 
@@ -31,6 +37,16 @@ public class CommentsReceiver {
 
     public Single<Map<String, Collection<JsonObject>>> groupCommentsByDomain(Observable<JsonObject> comments) {
         return comments.toMultimap(json -> getDomainFromEmail(json.getString("email")));
+    }
+
+    public Completable createDirectoryForDomain(String domain) {
+        return vertx.fileSystem()
+                .rxMkdirs(COMMENTS_PATH + domain);
+    }
+
+    public Completable writeCommentsToFile(String domain, Collection<JsonObject> comments) {
+        return vertx.fileSystem()
+                .rxWriteFile(COMMENTS_PATH + domain + "/comments.txt", Buffer.buffer(comments.toString()));
     }
 
     private String getDomainFromEmail(String email) {
